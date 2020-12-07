59d061e322bed6dcfd2d41dee2b287c8dcdf09eb
Darek2912 <60610617+Darek2912@users.noreply.github.com>
2020-12-07 17:44:53 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,59 +1,14 @@
-def load_image(filename):
-    infile = open(filename, "r")
-    instr = infile.read().split('\n')
-    image["type"] = instr.pop(0)
-    if image["comment"]:
-        image["comment"] = instr.pop(0)
-    image["size"] = list(map(int, instr.pop(0).split(' ')))
-    for row in instr:
-        image["pixels"].append(list(map(int, row.split(' '))))
-    return image
+def load_img(file_name):
+  #  plik = open(file_name)
+    plik = open('zdam2.txt', 'w')
+    return plik     #zeby zwrocic do zmiennej plik ktory zostal otwarty
 
+def save_img(file_name, dane):
+    plik = open(file_name, 'w')
+    plik.write(dane)
 
-def generate_square(size, colour=True):
-    image = {}
-    image["type"] = "P1"
-    image["size"] = [size, size]
-    image["comment"] = ''
-    image["pixels"] = []
-    for row in range(size):
-        line = []
-        for col in range(size):
-            line.append(1)
-        image["pixels"].append(line)
-    return image
+img = load_img('zdam.txt')
 
-
-def generate_diagonal(size):
-    
-
-
-def mirror_y(image):
-    for col in range(size)
-       if image["pixels"][col] == 0
-          image["pixels"][col] == 1
-       else:
-          image["pixels"][col] == 0 
-    return image 
-
-
-def mirror_x(image):
-    pass
-
-
-def save_image(filename, image):
-    """
-        Saves an image to a filename.
-    """
-
-    outfile = open(filename, "w+")  # Open a filename in a write mode
-    outfile.write(image["type"] + "\n")
-    if image["comment"]:
-        outfile.write("# " + image["comment"] + "\n")  # Add comment if there is any
-    outfile.write(' '.join(list(map(str, image["size"]))) + '\n')   # Change ints to strs and join them with spaces
-    for row in image["pixels"]:
-        outfile.write(' '.join(list(map(str, row))) + '\n') # Change ints to strs and join them with spaces
-
-image = generate_square(3, True)
-# image = load_image("test.pgm")
-save_image("black.pgm", image)
+save_img("zdam.txt", 'razdwatrzys')
+print(img.readline())
+print('dupa')
