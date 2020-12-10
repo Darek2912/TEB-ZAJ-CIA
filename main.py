d26e565bef8c0e0558711ebcd3a1f0ae5089b4f5
Darek2912 <60610617+Darek2912@users.noreply.github.com>
2020-12-10 02:48:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-def load_img(file_name):
-  #  plik = open(file_name)
-    plik = open('zdam2.txt', 'w')
-    return plik     #zeby zwrocic do zmiennej plik ktory zostal otwarty
+def load_file(file_name):   #tworzenie funkcji ktora uploaduje plik i zwraca module write 
+    plik = open(file_name, "w")
+    return plik 
 
-def save_img(file_name, dane):
-    plik = open(file_name, 'w')
-    plik.write(dane)
+def save_to_file(file,dane): 
+    file.write(str(dane))   #tworzy funkcje ktora zapisuje dane do pliku ktory jest w module write 
 
-img = load_img('zdam.txt')
 
-save_img("zdam.txt", 'razdwatrzys')
-print(img.readline())
-print('dupa')
+file = load_file('zdam.txt')
+save_to_file(file,"dane")
